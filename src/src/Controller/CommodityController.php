7b10e58ec71645db59f6afb868927ba556761263
Ajay Surti <ajayksurti@gmail.com>
2020-05-10 14:13:52 +0100

    CRUD operation for Commodity controller - tested with /commodities resource

--- src/src/Controller/CommodityController.php
+++ src/src/Controller/CommodityController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\CommodityRepository;
+use App\Repository\FamilyRepository;
+use App\Repository\RawClassRepository;
+use App\Repository\SegmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,14 +22,26 @@ class CommodityController extends AbstractController
      * @var CommodityRepository
      */
     private $commodityRepository;
+    private $segmentRepository;
+    private $familyRepository;
+    private $rawClassRepository;
 
     /**
      * CommodityController constructor.
      * @param CommodityRepository $commodityRepository
+     * @param SegmentRepository $segmentRepository
+     * @param FamilyRepository $familyRepository
+     * @param RawClassRepository $rawClassRepository
      */
-    public function __construct(CommodityRepository $commodityRepository)
+    public function __construct(CommodityRepository $commodityRepository,
+        SegmentRepository $segmentRepository,
+        FamilyRepository $familyRepository,
+        RawClassRepository $rawClassRepository)
     {
         $this->commodityRepository = $commodityRepository;
+        $this->segmentRepository = $segmentRepository;
+        $this->familyRepository = $familyRepository;
+        $this->rawClassRepository = $rawClassRepository;
     }
 
     /**
@@ -36,11 +51,20 @@ class CommodityController extends AbstractController
      */
     public function getCommodity($commodityId): JsonResponse
     {
-        $commodity = null;
+        $commodity = $this->commodityRepository->findOneBy([
+            'id' => $commodityId
+        ]);
 
         if ($commodity) {
             return new JsonResponse([
-
+                'id' => $commodity->getId(),
+                'commodity' => $commodity->getName(),
+                'segment_id' => $commodity->getSegmentId()->getId(),
+                'segment' => $commodity->getSegmentId()->getName(),
+                'family_id' => $commodity->getFamilyId()->getId(),
+                'family' => $commodity->getFamilyId()->getName(),
+                'class_id' => $commodity->getRawClassId()->getId(),
+                'class' => $commodity->getRawClassId()->getName()
             ], Response::HTTP_OK);
         } else {
             return new JsonResponse([
@@ -56,9 +80,34 @@ class CommodityController extends AbstractController
      */
     public function create(Request $request): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Commodity - data created.'
-        ], Response::HTTP_CREATED);
+        $data = json_decode($request->getContent(), true);
+
+        if (empty($data['segment_id']) || empty($data['family_id']) || empty($data['class_id']) ||
+            empty($data['name'])) {
+            return new JsonResponse([
+                'message' => 'please provide accurate data to create commodity !'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        } else {
+            $commoditySegment = $this->segmentRepository->findOneBy(['id' => $data['segment_id']]);
+            $commodityFamily = $this->familyRepository->findOneBy(['id' => $data['family_id']]);
+            $commodityRawClass = $this->rawClassRepository->findOneBy(['id' => $data['class_id']]);
+            $commodityName = $data['name'];
+
+            if (empty($commoditySegment) || empty($commodityFamily) || empty($commodityRawClass)) {
+                return new JsonResponse([
+                    'message' => 'Required parameters for Segment or Family or Class field(s) are not aligned to create 
+                    the record ! !'
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $this->commodityRepository->saveCommodity($commoditySegment, $commodityFamily, $commodityRawClass,
+                $commodityName);
+
+            return new JsonResponse([
+                'message' => 'Commodity - data created.'
+            ], Response::HTTP_CREATED);
+        }
+
     }
 
     /**
@@ -69,9 +118,40 @@ class CommodityController extends AbstractController
      */
     public function update($commodityId, Request $request): JsonResponse
     {
-        return new JsonResponse([
+        $commodity = $this->commodityRepository->findOneBy(['id' => $commodityId]);
+        if ($commodity) {
+            $data = json_decode($request->getContent(), true);
+            if (empty($data['segment_id']) || empty($data['family_id']) || empty($data['class_id']) ||
+                empty($data['name'])) {
+                return new JsonResponse([
+                    'message' => 'please provide accurate data to create commodity !'
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            } else {
+                $commoditySegment = $this->segmentRepository->findOneBy(['id' => $data['segment_id']]);
+                $commodityFamily = $this->familyRepository->findOneBy(['id' => $data['family_id']]);
+                $commodityRawClass = $this->rawClassRepository->findOneBy(['id' => $data['class_id']]);
+                $commodityName = $data['name'];
 
-        ], Response::HTTP_OK);
+                if (empty($commoditySegment) || empty($commodityFamily) || empty($commodityRawClass)) {
+                    return new JsonResponse([
+                        'message' => 'Required parameters for Segment or Family or Class field(s) are not aligned to 
+                        create the record ! !'
+                    ], Response::HTTP_UNPROCESSABLE_ENTITY);
+                } else {
+                    $commodity->setName($commodityName);
+                    $commodity->setSegmentId($commoditySegment);
+                    $commodity->setFamilyId($commodityFamily);
+                    $commodity->setRawClassId($commodityRawClass);
+                    $updatedCommodity = $this->commodityRepository->updateCommodity($commodity);
+
+                    return new JsonResponse([
+                        $updatedCommodity->toArray()
+                    ], Response::HTTP_OK);
+                }
+            }
+        } else {
+            return new JsonResponse([], Response::HTTP_NOT_FOUND);
+        }
     }
 
     /**
@@ -81,9 +161,10 @@ class CommodityController extends AbstractController
      */
     public function delete($commodityId): JsonResponse
     {
-        $commodity = null;
+        $commodity = $this->commodityRepository->findOneBy(['id' => $commodityId]);
 
         if ($commodity) {
+            $this->commodityRepository->removeCommodity($commodity);
             return new JsonResponse([
                 'message' => 'Commodity - data removed.'
             ], Response::HTTP_NO_CONTENT);
@@ -100,7 +181,20 @@ class CommodityController extends AbstractController
      */
     public function getAll(): JsonResponse
     {
+        $commodities = $this->commodityRepository->findAll();
         $data = [];
+        foreach ($commodities as $commodity) {
+            $data[] = [
+                'id' => $commodity->getId(),
+                'commodity' => $commodity->getName(),
+                'segment_id' => $commodity->getSegmentId()->getId(),
+                'segment' => $commodity->getSegmentId()->getName(),
+                'family_id' => $commodity->getFamilyId()->getId(),
+                'family' => $commodity->getFamilyId()->getName(),
+                'class_id' => $commodity->getRawClassId()->getId(),
+                'class' => $commodity->getRawClassId()->getName()
+            ];
+        }
 
         return new JsonResponse($data, Response::HTTP_OK);
     }
